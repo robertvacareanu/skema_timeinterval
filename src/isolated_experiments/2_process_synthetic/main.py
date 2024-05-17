b19c93eb6a6b00fb997cd8c8f41c3401b43fc079
Robert Vacareanu <robert.vacareanu@gmail.com>
2024-05-17 14:40:00 -0700

    New synthetic data

--- src/isolated_experiments/2_process_synthetic/main.py
+++ src/isolated_experiments/2_process_synthetic/main.py
@@ -10,3 +10,15 @@ if __name__== "__main__":
 
     with open('data/synthetic/initial_data.jsonl', 'w+') as fout:
         json.dump(data_parsed, fout, indent=4)
+
+
+    data = []
+    with open('data/synthetic_original/data_closer_to_domain.jsonl') as fin:
+        for line in fin:
+            data.append(json.loads(line))
+    
+    data_parsed = [parse_synthetic_data(x) for x in data]
+    data_parsed = [x for x in data_parsed if x]
+
+    with open('data/synthetic/data_closer_to_domain.jsonl', 'w+') as fout:
+        json.dump(data_parsed, fout, indent=4)
